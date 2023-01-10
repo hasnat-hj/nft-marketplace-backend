449040eebccb938aabaa7fff5fe83c104abce253
sulaiman-s <sulaimanfarooqi526@gmail.com>
2023-01-10 16:22:36 +0500

    cloudinary update

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const AuctionItemRoute = require("./routes/auctionRouter");
 const ActivitiesRoute = require("./routes/activityRouter");
 const Userrouter = require("./routes/UserRoutes");
 
-
 //connect to mongodb ..
 mongoose
   .connect(process.env.DB_CONNECT)
@@ -35,7 +34,7 @@ mongoose
 app.use("/nft", NftItemRoute);
 app.use("/Anft", AuctionItemRoute);
 app.use("/activity", ActivitiesRoute);
-app.use('/user',Userrouter)
+app.use("/user", Userrouter);
 
 //connect to server
 app.listen(PORT, () => console.log(`Server connected on port ${PORT}`));
--- routes/auctionRouter.js
+++ routes/auctionRouter.js
@@ -1,67 +1,74 @@
 const router = require("express").Router();
 const AuctionNFTs = require("../models/auctionNftModel");
+const path = require("path");
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
+cloudinary.config({
+  cloud_name: "mansoorcloud",
+  api_key: "475838235114589",
+  api_secret: "VqT5klF59dCMOLr58Xsnk2syymk",
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: multer.diskStorage({}),
+});
 
-router.post("/createAuctionNft", upload.single("nftImage"), (req, res) => {
-  const saveImage =  AuctionNFTs({
-    id: req.body.id,
-    name: req.body.name,
-    minbid: req.body.minbid,
-    curbid: req.body.curbid,
-    duration: req.body.duration,
-    img:  req.file.filename,
-    isBuy: req.body.isBuy,
-    owner: req.body.owner
-  });
-  saveImage
-    .save()
-    .then((res) => {
-      console.log("image is saved");
-    })
-    .catch((err) => {
-      console.log(err, "error has occur");
+router.post(
+  "/createAuctionNft",
+  upload.single("nftImage"),
+  async (req, res) => {
+    const result = await cloudinary.uploader.upload(req.file.path);
+    const saveImage = AuctionNFTs({
+      id: req.body.id,
+      name: req.body.name,
+      minbid: req.body.minbid,
+      curbid: req.body.curbid,
+      duration: req.body.duration,
+      img: result.secure_url,
+      isBuy: req.body.isBuy,
+      owner: req.body.owner,
     });
-    res.send('image is saved')
-});
+    saveImage
+      .save()
+      .then((res) => {
+        console.log("image is saved");
+      })
+      .catch((err) => {
+        console.log(err, "error has occur");
+      });
+    res.send(saveImage);
+  }
+);
 
 //update item
-router.put('/createAuctionNft/:id', async (req, res)=>{
-  try{
+router.put("/createAuctionNft/:id", async (req, res) => {
+  try {
     //find the item by its id and update it
     // NFTs.findByIdAndUpdate()
-    const updateItem = await AuctionNFTs.findByIdAndUpdate(req.params.id, {   curbid: req.body.curbid });
+    const updateItem = await AuctionNFTs.findByIdAndUpdate(req.params.id, {
+      curbid: req.body.curbid,
+    });
     res.status(200).json(updateItem);
-  }catch(err){
+  } catch (err) {
     res.json(err);
   }
-})
-router.get('/getAuctionNft/:id', async (req, res)=>{
-  try{
+});
+router.get("/getAuctionNft/:id", async (req, res) => {
+  try {
     //find the item by its id and update it
     // NFTs.findByIdAndUpdate()
     const Item = await AuctionNFTs.findById(req.params.id);
     res.status(200).json(Item);
-  }catch(err){
+  } catch (err) {
     res.json(err);
   }
-})
-
+});
 
 router.get("/getAuctionNft", async (req, res, next) => {
   try {
-    const nfts = await AuctionNFTs.find({isBuy:false});
+    const nfts = await AuctionNFTs.find({ isBuy: false });
     // console.log("nfts: " + nfts);
     if (!nfts) res.status(400).json({ msg: "No data found" });
     res.status(200).json(nfts);
